2c7705ea8ff1b3c4d9ceacf9c8c9a00a71184425
Tim Redmond <tim.redmond@canopytax.com>
2016-05-23 12:31:40 -0600

    Update code to use PSR-2 standards with validation through Code Sniffer

--- src/JQL.php
+++ src/JQL.php
@@ -1,95 +1,106 @@
-<?php namespace Canopy\JQL;
+<?php
+namespace Canopy\JQL;
 
+use stdClass;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class JQL
 {
-	protected $model;
-	protected $query;
-	protected $operatorMap = [
-		'lt' => '<',
-		'gt' => '>',
-		'lte' => '<=',
-		'gte' => '>=',
-		'eq' => '=',
-		'ne' => '!=',
-		'beginswith' => 'beginswith',
-		'endswith' => 'endswith',
-		'contains' => 'contains',
-		'in' => 'in',
-	];
+    protected $model;
+    protected $query;
+    protected $operatorMap = [
+        'lt' => '<',
+        'gt' => '>',
+        'lte' => '<=',
+        'gte' => '>=',
+        'eq' => '=',
+        'ne' => '!=',
+        'beginswith' => 'beginswith',
+        'endswith' => 'endswith',
+        'contains' => 'contains',
+        'in' => 'in',
+    ];
 
-	public function __construct(Model $model)
-	{
-		$this->model = $model;
-		$this->query = $model->query();
-	}
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+        $this->query = $model->query();
+    }
 
-	public function convertToFluent($json)
-	{
-		$json = json_decode($json);
+    public function convertToFluent($json)
+    {
+        $json = json_decode($json);
 
-		/** @var Builder $fluent */
-		$query = $this->parseJQL($json->jql, $this->query);
+        $query = $this->parseJQL($json->jql, $this->query);
 
-		return $query;
-	}
+        return $query;
+    }
 
-	public function parseJQL($jql, $query, $binder = "AND")
-	{
-		$count = 0;
-		foreach ($jql as $item) {
+    /**
+     * @param array $jql
+     * @param Builder $query
+     * @param string $binder
+     * @return mixed
+     */
+    public function parseJQL($jql, $query, $binder = 'AND')
+    {
+        $count = 0;
+        foreach ($jql as $item) {
+            // If "OR" then iterate through and bind them through orWhere's
+            if ($item instanceof stdClass && property_exists($item, 'OR')) {
+                if ($count == 0) {
+                    $this->parseJQL($item->OR, $query, 'OR');
+                } else {
+                    $query->where(
+                        function ($query) use ($item) {
+                            $this->parseJQL($item->OR, $query, 'OR');
+                        }
+                    );
+                }
+            } else {
+                if ($binder == 'OR') {
+                    if (is_array($item)) {
+                        $query->orWhere(
+                            function ($query) use ($item) {
+                                $this->parseJQL($item, $query);
+                            }
+                        );
+                    } else {
+                        $query->orWhere($item->field, $this->operatorMap[$item->operator], $item->value);
+                    }
+                } else {
+                    if (is_array($item)) {
+                        $query->where(
+                            function ($query) use ($item) {
+                                $this->parseJQL($item, $query);
+                            }
+                        );
+                    } else {
+                        $query->where($item->field, $this->operatorMap[$item->operator], $item->value);
+                    }
+                }
+            }
 
-			// If "OR" then itterate through and bind them through orWhere's
-			if ($item instanceof \stdClass && property_exists($item, 'OR')) {
-				if ($count == 0) {
-					$this->parseJQL($item->OR, $query, "OR");
-				} else {
-					$query->where(function($query) use ($item) {
-						$this->parseJQL($item->OR, $query, "OR");
-					});
-				}
-			} else {
-				if ($binder == "OR") {
-					if (is_array($item)) {
-						$query->orWhere(function ($query) use ($item) {
-							$this->parseJQL($item, $query);
-						});
-					} else {
-						$query->orWhere($item->field, $this->operatorMap[$item->operator], $item->value);
-					}
-				} else {
-					if (is_array($item)) {
-						$query->where(function ($query) use ($item) {
-							$this->parseJQL($item, $query);
-						});
-					} else {
-						$query->where($item->field, $this->operatorMap[$item->operator], $item->value);
-					}
-				}
-			}
-
-			$count++;
-		}
-		return $query;
-	}
+            $count++;
+        }
+        return $query;
+    }
 
 
-	/**
-	 * @return Model
-	 */
-	public function getModel()
-	{
-		return $this->model;
-	}
+    /**
+     * @return Model
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
 
-	/**
-	 * @param Model $model
-	 */
-	public function setModel($model)
-	{
-		$this->model = $model;
-	}
-
+    /**
+     * @param Model $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
 }
